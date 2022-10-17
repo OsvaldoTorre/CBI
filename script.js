466e735dffca34f8ca4c26cd2fc3030268cdd797
Osvaldo Abraham de la Torre Ortiz <99097258+OsvaldoTorre@users.noreply.github.com>
2022-10-17 08:42:41 -0500

    Actualización metodo
    se actualizo el metodo de la función de densidad y la función de media

--- script.js
+++ script.js
@@ -57,14 +57,84 @@ function addRow(tableID, elemn) {
   }
 }
 
-function realizarCalculos(params) {
-  
+function fDensidad(media,varianza) {
+  res=(1/(varianza*(Math.sqrt(2*Math.PI))))*Math.exp((-(1/2))*(Math.pow(((X-media)/varianza),2)));
 }
 
-function media(params) {
+function media(patrones) {
+  cds0,cds1,cds2,cds3=0;
+  cdc0,cdc1,cdc2,cdc3=0;
+  cdv0,cdv1,cdv2,cdv3=0;
 
-}
+  for (let fila = 0; fila < patrones.length; fila++) {
 
-function varianza(params) {
+    for (let celda = 0; celda < patrones[fila].length; celda++) {
+        if (fila<=49) {
+          //setosa
+          if (celda==0) {
+            cds0=cds0+patrones[fila][celda];
   
+          }else if (celda==1) {
+            cds1=cds1+patrones[fila][celda];
+  
+          }else if (celda==2) {
+            cds2=cds2+patrones[fila][celda];
+  
+          }else if (celda==3) {
+            cds3=cds3+patrones[fila][celda];
+  
+          }
+
+        } else if (fila>49&&fila<=99) {
+          //color
+          if (celda==0) {
+            cdc0=cdc0+patrones[fila][celda];
+  
+          }else if (celda==1) {
+            cdc1=cdc1+patrones[fila][celda];
+  
+          }else if (celda==2) {
+            cdc2=cdc2+patrones[fila][celda];
+  
+          }else if (celda==3) {
+            cdc3=cdc3+patrones[fila][celda];
+  
+          }
+
+        } else if (fila >= 99&& fila<=149) {
+          //virginica
+          if (celda==0) {
+            cdv0=cdv0+patrones[fila][celda];
+  
+          }else if (celda==1) {
+            cdv1=cdv1+patrones[fila][celda];
+  
+          }else if (celda==2) {
+            cdv2=cdv2+patrones[fila][celda];
+  
+          }else if (celda==3) {
+            cdv3=cdv3+patrones[fila][celda];
+  
+          }
+
+        }
+    }
+    
+  }
+
+  mediaC0S = cds0/50
+  mediaC1S = cds1/50
+  mediaC2S = cds2/50
+  mediaC3S = cds3/50
+
+  mediaC0C = cds0/50
+  mediaC1C = cds1/50
+  mediaC2C = cds2/50
+  mediaC3C = cds3/50
+
+  mediaC0V = cds0/50
+  mediaC1V = cds1/50
+  mediaC2V = cds2/50
+  mediaC3V = cds3/50
+
 }
